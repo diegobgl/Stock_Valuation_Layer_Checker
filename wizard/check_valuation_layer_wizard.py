bb855818d49d20eb2e406d8aef03302a9f4f8fd2
Diego Gajardo <diegobgajardo@gmail.com>
2025-01-09 12:58:57 -0300

    ss

--- wizard/check_valuation_layer_wizard.py
+++ wizard/check_valuation_layer_wizard.py
@@ -4,10 +4,15 @@ class CheckValuationLayerWizard(models.TransientModel):
     _name = 'check.valuation.layer.wizard'
     _description = 'Buscar Stock Valuation Layers Sin Asientos Contables'
 
+    start_date = fields.Date(string='Fecha Inicio', required=True)
+    end_date = fields.Date(string='Fecha Fin', required=True)
+
     def search_valuation_layers_without_accounting(self):
         """Buscar Stock Valuation Layers sin asientos contables y con ubicaciones de tipo producción."""
         domain = [
             ('account_move_id', '=', False),  # Sin asiento contable
+            ('create_date', '>=', self.start_date),  # Fecha inicio
+            ('create_date', '<=', self.end_date),    # Fecha fin
             '|',  # Condición OR para considerar ambas ubicaciones (origen/destino)
             ('stock_move_id.location_id.usage', '=', 'production'),  # Ubicación origen de tipo producción
             ('stock_move_id.location_dest_id.usage', '=', 'production')  # Ubicación destino de tipo producción
