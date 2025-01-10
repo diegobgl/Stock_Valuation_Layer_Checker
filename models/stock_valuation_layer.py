4089b4086de647c3ec9b1eff7ce12109b18f4e84
Diego Gajardo <diegobgajardo@gmail.com>
2025-01-10 09:34:11 -0300

    sss

--- models/stock_valuation_layer.py
+++ models/stock_valuation_layer.py
@@ -4,8 +4,8 @@ class StockValuationLayer(models.Model):
     _inherit = 'stock.valuation.layer'
 
     def action_create_accounting_entries(self):
-        """Reutiliza la acción nativa para crear los asientos contables."""
-        for layer in self:
-            if not layer.account_move_id:
-                # Reutilizamos el método nativo para crear el asiento contable
-                layer._create_accounting_entries()
+        """
+        Crear asientos contables para las capas seleccionadas.
+        Este método llama a `_validate_accounting_entries` para generar las entradas.
+        """
+        self._validate_accounting_entries()
